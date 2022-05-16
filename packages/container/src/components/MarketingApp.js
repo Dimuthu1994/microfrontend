3175b397a8afb2a6c2883327787e075f66c6ecce
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-05-16 16:00:51 +0530

    init

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, { useRef, useEffect } from "react";
+import { mount } from "marketing/MarketingApp";
+
+function MarketingApp(props) {
+  const ref = useRef(null);
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+}
+
+export default MarketingApp;
